e0cdb3dfef877edc015a1c4e4de4a383405c8821
TanighClark <137099207+TanighClark@users.noreply.github.com>
2025-05-06 19:11:19 -0400

    Remove vite.svg import

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,3 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-
 import Navbar from './components/Navbar';
 import { Routes, Route } from 'react-router-dom';
 import WordEntry from './pages/WordEntry';
