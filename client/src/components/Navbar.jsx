bdd7ef93a2248619f7bf4abcec11847987592c78
TanighClark <137099207+TanighClark@users.noreply.github.com>
2025-05-06 17:27:56 -0400

    Finished home page

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,22 +1,35 @@
 import { Link } from 'react-router-dom';
-import '../pages/styles/Navbar.css'; // Import your CSS file
+import { useState } from 'react';
+import '../pages/styles/Navbar.css';
 
 export default function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <nav className="navbar">
-      <div className="navbar-logo">Spell & Play</div>
-      <ul className="navbar-links">
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/activities">Activities</Link>
-        </li>
-        <li>
-          <Link to="/help">Help</Link>
-        </li>{' '}
-        {/* Placeholder for future use */}
-      </ul>
+      <div className="navbar-content">
+        <div className="navbar-logo">Spell & Play</div>
+        <button className="navbar-toggle" onClick={() => setIsOpen(!isOpen)}>
+          ☰
+        </button>
+        <ul className={`navbar-links ${isOpen ? 'open' : ''}`}>
+          <li>
+            <Link to="/" onClick={() => setIsOpen(false)}>
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link to="/activities" onClick={() => setIsOpen(false)}>
+              Activities
+            </Link>
+          </li>
+          <li>
+            <Link to="/help" onClick={() => setIsOpen(false)}>
+              Help
+            </Link>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 }
