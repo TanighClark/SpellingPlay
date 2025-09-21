dcd3efa81b89683ac4ace6fbc532011dccf363e3
TanighClark <137099207+TanighClark@users.noreply.github.com>
2025-09-21 21:56:05 +0000

    Tailwind: compile with Vite

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
+import './styles/tailwind.css';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
